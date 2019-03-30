e511b369451d606950992d16221c95dbf2f6faf1
nyhilo <silentclowd@gmail.com>
2019-03-30 12:49:59 -0700

    Starting over from scratch. Going to properly use telegram.ext api if it kills me

---
+++ caroline.py
@@ -0,0 +1,42 @@
+#
+# Caroline is a telegram chatbot
+# Nyhilo 2019-3-30
+#
+
+import logging
+
+
+
+###########
+# logging #
+###########
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+###########
+# Globals #
+###########
+
+
+
+#####################
+# Utility Functions #
+#####################
+
+
+
+
+#############
+# Functions #
+#############
+
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
